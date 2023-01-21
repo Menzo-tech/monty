136074f7cd8550da9e581e0afb023070770a8e53
Mmenim <inyangmmenim@gmail.com>
2023-01-21 14:13:01 -0800

    refractor code

---
+++ stack_func.c
@@ -0,0 +1,111 @@
+#include "monty.h"
+/**
+ * _push - push int to a stack
+ * @stack: linked lists for monty stack
+ * @line_number: number of line opcode occurs on
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new;
+	char *arg;
+	int push_arg;
+
+	push_arg = 0;
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		printf("Error: malloc failed\n");
+		error_exit(stack);
+	}
+
+	arg = strtok(NULL, "\n ");
+	if (isnumber(arg) == 1 && arg != NULL)
+	{
+		push_arg = atoi(arg);
+	}
+	else
+	{
+		printf("L%d: usage: push integer\n", line_number);
+		error_exit(stack);
+	}
+
+	if (sq_flag == 1)
+	{
+		add_dnodeint_end(stack, push_arg);
+	}
+
+	if (sq_flag == 0)
+	{
+		add_dnodeint(stack, push_arg);
+	}
+
+}
+/**
+ * _pall - print all function
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+void _pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+{
+	stack_t *runner;
+
+	runner = *stack;
+	while (runner != NULL)
+	{
+		printf("%d\n", runner->n);
+		runner = runner->next;
+	}
+}
+/**
+ * _pint - print int a top of stack
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ *
+ */
+void _pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *runner;
+
+	runner = *stack;
+	if (runner == NULL)
+	{
+		printf("L%d: can't pint, stack empty\n", line_number);
+		error_exit(stack);
+	}
+	printf("%d\n", runner->n);
+}
+/**
+ * _swap - swap top of stack and second top of stack
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ *
+ */
+void _swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *runner;
+	int tmp;
+
+	runner = *stack;
+	if (runner == NULL || runner->next == NULL)
+	{
+		printf("L%d: can't swap, stack too short\n", line_number);
+		error_exit(stack);
+	}
+	tmp = runner->n;
+	runner->n = runner->next->n;
+	runner->next->n = tmp;
+}
+/**
+ * _pop - delete item at top of stack
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		printf("L%d: can't pop an empty stack\n", line_number);
+		error_exit(stack);
+	}
+	delete_dnodeint_at_index(stack, 0);
+}
